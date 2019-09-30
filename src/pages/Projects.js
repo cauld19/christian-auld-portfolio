643e6c532a31b937a85cfb65e3e15a65795f1d05
cauld19 <50849710+cauld19@users.noreply.github.com>
2019-09-30 00:31:20 -0700

    final commit tonight

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,7 +10,7 @@ export default function Projects () {
 
     return (
         <Container style={{marginTop:"10%"}}>
-            <p className="w-75 mx-auto" style={{marginBottom:"10%"}}>Below are a few of the most recent projects that I have completed. As I complete more (hopefully better) projects, those displayed will change. For all of my projects: <a href="https://github.com/cauld19">Github</a>.</p>
+            <p className="w-75 mx-auto" style={{marginBottom:"10%"}}>Below are a few of the most recent projects that I have completed. As I complete more (hopefully impressive) projects, those displayed will change. For all of my projects: <a href="https://github.com/cauld19">Github</a>.</p>
             <Row>
                 <ProjectCardOne />
                 <ProjectCardTwo />
